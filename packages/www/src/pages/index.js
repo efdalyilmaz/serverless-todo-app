fe9661088876ab0f0b51bb324f4e2857b61500e7
Efdal Yılmaz <yilmazefdal@gmail.com>
2022-01-30 00:42:22 +0300

    efdal fixes packages && some bugs

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,28 +1,30 @@
 import React, { useEffect } from "react";
-import { Container, Heading, Button} from "theme-ui";
+import { Container, Flex, Heading, Button} from "theme-ui";
 import netlifyIdentity from "netlify-identity-widget";
 
 
-export default props => (
+export default props => {
     useEffect( () => {
         netlifyIdentity.init({});
     });
 
-    <Container>
-        <Flex sx={{flexDirection:"column", padding:3}}>
-            <Heading as="h1">Get Stuff Done</Heading>
-            <Button 
-                sx={{marginTop:2}}
-                onClick={() => { netlifyIdentity.open()}}
-            >
-                Log In
-            </Button>
-            <Button 
-                sx={{marginTop:2}}
-                onClick={() => { netlifyIdentity.currentUser()}}
-            >
-                Log User
-            </Button>
-        </Flex>
-    </Container>
-);
+    return (
+        <Container>
+            <Flex sx={{ flexDirection:"column", padding:3 }}>
+                <Heading as="h1">Get Stuff Done</Heading>
+                <Button 
+                    sx={{marginTop:2}}
+                    onClick={() => { netlifyIdentity.open()}}
+                >
+                    Log In
+                </Button>
+                <Button 
+                    sx={{marginTop:2}}
+                    onClick={() => { netlifyIdentity.currentUser()}}
+                >
+                    Log User
+                </Button>
+            </Flex>
+        </Container>
+    );
+}
