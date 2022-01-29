49f6cdd0530e453b071307b1dbfb521060c6facb
Efdal Yılmaz <yilmazefdal@gmail.com>
2022-01-30 00:24:22 +0300

    efdal adds netlify-identity-widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button} from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
+
 
 export default props => (
+    useEffect( () => {
+        netlifyIdentity.init({});
+    });
+
     <Container>
         <Flex sx={{flexDirection:"column", padding:3}}>
             <Heading as="h1">Get Stuff Done</Heading>
             <Button 
                 sx={{marginTop:2}}
-                onClick={() => { alert(clicked);}}
+                onClick={() => { netlifyIdentity.open()}}
             >
                 Log In
+            </Button>
+            <Button 
+                sx={{marginTop:2}}
+                onClick={() => { netlifyIdentity.currentUser()}}
+            >
+                Log User
             </Button>
         </Flex>
     </Container>
