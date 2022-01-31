0aa0896ed3b8c2b3ad4d2e63da2a30b40f84a635
Efdal Yılmaz <yilmazefdal@gmail.com>
2022-01-31 23:43:27 +0300

    efdal adds NavLink

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,15 +1,28 @@
 import React, { useEffect } from "react";
-import { Container, Flex, Heading, Button} from "theme-ui";
+import { Container, Flex, Heading, Button, NavLink} from "theme-ui";
+import {Link} from "gatsby";
 import netlifyIdentity from "netlify-identity-widget";
 
 
 export default props => {
     useEffect( () => {
         netlifyIdentity.init({});
+        
     });
 
     return (
         <Container>
+            <Flex as="nav">
+                <NavLink as={Link} to={"/"} href="#!" p={2}>
+                    Home
+                </NavLink>
+                <NavLink as={Link} to={"/app"} href="#!" p={2}>
+                    Blog
+                </NavLink>
+                <NavLink href="#!" p={2}>
+                    About
+                </NavLink>
+            </Flex>
             <Flex sx={{ flexDirection:"column", padding:3 }}>
                 <Heading as="h1">Get Stuff Done</Heading>
                 <Button 
