c2ebb640225275160b182e5db1bede270bf6b8bd
Efdal Yılmaz <yilmazefdal@gmail.com>
2022-01-31 23:55:33 +0300

    efdal adds useState hooks for user

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Flex, Heading, Button, NavLink} from "theme-ui";
 import {Link} from "gatsby";
 import netlifyIdentity from "netlify-identity-widget";
 
 
 export default props => {
+    const [user, setUser] = useState();
+
     useEffect( () => {
         netlifyIdentity.init({});
-        
+        netlifyIdentity.on("login", user => { 
+            netlifyIdentity.close(); 
+            setUser(user)
+        });
+        netlifyIdentity.on("logout", () => setUser());
     });
 
     return (
@@ -19,9 +25,11 @@ export default props => {
                 <NavLink as={Link} to={"/app"} href="#!" p={2}>
                     Blog
                 </NavLink>
-                <NavLink href="#!" p={2}>
-                    About
-                </NavLink>
+                { user && 
+                    (<NavLink href="#!" p={2}>
+                        {user.user_metadata.full_name}
+                    </NavLink>)
+                }
             </Flex>
             <Flex sx={{ flexDirection:"column", padding:3 }}>
                 <Heading as="h1">Get Stuff Done</Heading>
