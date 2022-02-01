8d84f363a660991920de1ebb7063b9f98456898c
Efdal Yılmaz <yilmazefdal@gmail.com>
2022-02-02 00:25:31 +0300

    efdal adds identiy-context

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,51 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { Container, Flex, Heading, Button, NavLink} from "theme-ui";
-import {Link} from "gatsby";
-import netlifyIdentity from "netlify-identity-widget";
-
-
+import React, { useContext } from "react";
+import { Container, Heading, Button, Flex, NavLink } from "theme-ui";
+import { Link } from "gatsby";
+import { IdentityContext } from "../../identity-context";
 export default props => {
-    const [user, setUser] = useState();
+  const { user, identity: netlifyIdentity } = useContext(IdentityContext);
 
-    useEffect( () => {
-        netlifyIdentity.init({});
-        netlifyIdentity.on("login", user => { 
-            netlifyIdentity.close(); 
-            setUser(user)
-        });
-        netlifyIdentity.on("logout", () => setUser());
-    });
-
-    return (
-        <Container>
-            <Flex as="nav">
-                <NavLink as={Link} to={"/"} href="#!" p={2}>
-                    Home
-                </NavLink>
-                <NavLink as={Link} to={"/app"} href="#!" p={2}>
-                    Blog
-                </NavLink>
-                { user && 
-                    (<NavLink href="#!" p={2}>
-                        {user.user_metadata.full_name}
-                    </NavLink>)
-                }
-            </Flex>
-            <Flex sx={{ flexDirection:"column", padding:3 }}>
-                <Heading as="h1">Get Stuff Done</Heading>
-                <Button 
-                    sx={{marginTop:2}}
-                    onClick={() => { netlifyIdentity.open()}}
-                >
-                    Log In
-                </Button>
-                <Button 
-                    sx={{marginTop:2}}
-                    onClick={() => { netlifyIdentity.currentUser()}}
-                >
-                    Log User
-                </Button>
-            </Flex>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={"/app"} p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink href="#!" p={2}>
+            {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
