ef79af9bfcf396859710be54ac4fe6e45c775967
Efdal Yılmaz <yilmazefdal@gmail.com>
2022-02-02 00:22:07 +0300

    efdal adds new pages

--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -1,12 +1,13 @@
 const React = require("react");
 const { ThemeProvider } = require("theme-ui");
-const {deep} = require("@theme-ui/presets");
-
+const { deep } = require("@theme-ui/presets");
+const { Provider } = require("./identity-context");
 const tokens = {
-    ...deep,
-    size : {container:1024}
-}
-
-module.exports =  ({element}) => (
+  ...deep,
+  sizes: { container: 1024 }
+};
+module.exports = ({ element }) => (
+  <Provider>
     <ThemeProvider theme={tokens}>{element}</ThemeProvider>
+  </Provider>
 );
